0e9b6b02a552c3514ac852b3f97c87be8989b8a3
lookiyam <56125496+lookiyam@users.noreply.github.com>
2020-11-06 12:49:34 +0100

    dd

---
+++ Python course/slice.py
@@ -0,0 +1,29 @@
+parrot = "Norwegian Blue"
+
+print()
+
+# start stop stip
+
+print(parrot[0:6]) #Norweg, LAST NOT INCLUDED  , FIRST INCLUDED
+print(parrot[3:5]) # we
+print(parrot[0:9]) # Norwegian 
+print(parrot[:9])  #Norwegian with no start
+
+print()
+
+print(parrot[10:14])
+print(parrot[10:])
+
+print()
+
+print(parrot[:6])
+print(parrot[6:])
+
+print()
+
+print(parrot[:6] + parrot[6:]) # No specific value = Start or End
+
+print()
+
+print(parrot[:]) # No start no end = ALL
+
